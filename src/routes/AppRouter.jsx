33d4678d545da4de743375d11356dc8f2b6ebd40
jerson100 <juamkoo@gmail.com>
2020-09-28 19:55:47 -0500

    Build x2 - Corrigiendo el error del path

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  /*BrowserRouter as Router, */ Switch,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import Presentation from "../pages/Presentation";
 import Home from "../pages/Home";
 import Error from "../pages/Error";
 
 const AppRouter = () => {
   return (
-    <Router>
+    <HashRouter basename="/">
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/presentation" exact component={Presentation} />
         <Route component={Error} />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 };
 
