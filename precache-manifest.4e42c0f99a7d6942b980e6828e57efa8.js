b21e839edfff17268246a7652247d23f0434f147
jerson100 <juamkoo@gmail.com>
2020-09-28 20:11:03 -0500

    Updates

---
+++ precache-manifest.4e42c0f99a7d6942b980e6828e57efa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04cb99130f90075682912c1109ba876d",
+    "url": "/BalancedScorecard/index.html"
+  },
+  {
+    "revision": "bc86d81c76fa68a2619c",
+    "url": "/BalancedScorecard/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "e340a605ceef74ec87b1",
+    "url": "/BalancedScorecard/static/css/main.737343dd.chunk.css"
+  },
+  {
+    "revision": "bc86d81c76fa68a2619c",
+    "url": "/BalancedScorecard/static/js/2.31288abd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/BalancedScorecard/static/js/2.31288abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e340a605ceef74ec87b1",
+    "url": "/BalancedScorecard/static/js/main.cd5f2521.chunk.js"
+  },
+  {
+    "revision": "c792c66041ebd4bce0c6",
+    "url": "/BalancedScorecard/static/js/runtime-main.a90e0bf1.js"
+  }
+]);
